d20e613d16a49637bfd4bf18b8a8d45612e08d60
peisun <peisun.2013@gmail.com>
2016-02-03 16:40:49 -0800

    .

---
+++ lisp/l.lisp
@@ -0,0 +1,15 @@
+(* 1 (/ 27.5 12))
+'(this is a quote)
+fill-column
+(defun mul8 (number)
+  (interactive "p")
+  (* 8 number))
+(mul8 2)
+(if 0
+    'true
+  'false)
+(point)
+(mark)
+(cons 'pine '(rose violet daisy buttercup))
+load-path
+
